d73a37afce8065f1777abd9101da07422de67ce3
Wenmeng Liu <crystallwm2011@gmail.com>
2023-10-06 15:59:53 -0700

    DB connection setup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/wenmengliu/go-rest-api-course/internal/db"
+)
 
 // Run - is going to be responsible for
 // the instatiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wenmengliu/go-rest-api-course
 
 go 1.15
 
-require github.com/jmoiron/sqlx v1.3.5
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9 // indirect
+)
